b50bf20341c106fb1663babce626248a835718d0
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2022-12-06 00:41:45 -0300

    eliminar copia para array auxiliar

--- merge_sort.c
+++ merge_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mergeSort(int *vetor, int esq, int dir) {
+void mergeSort(int *vetor, int *vetor_aux, int esq, int dir) {
   if(esq == dir) { //se o elemento da esquerda for igual ao da direita, o vetor possui uma posição
     return;
   }
@@ -11,22 +11,22 @@ void mergeSort(int *vetor, int esq, int dir) {
     int meio = (esq + dir) / 2;
 
     //chama a mesma função para as duas metades do array
-    mergeSort(vetor, esq, meio);
-    mergeSort(vetor, meio+1, dir);
+    mergeSort(vetor_aux, vetor, esq, meio);
+    mergeSort(vetor_aux, vetor, meio+1, dir);
 
     if(vetor[meio] <= vetor[meio+1]) { //impedir merge caso seja desnecessário
       return;
     }
     //função que combinará dois arrays
-    merge(vetor, esq, meio, dir);
-  } 
-     
-}
-void merge(int *vetor, int esq, int meio, int dir) {
-  int i, j, k; //contadores
-  int *vetor_aux;
+    merge(vetor, vetor_aux, esq, meio, dir);
+  }
 
-  vetor_aux = (int *) malloc(sizeof(int) * dir+1); //alocação de memória para o vetor auxiliar
+}
+void merge(int *vetor, int *vetor_aux, int esq, int meio, int dir) {
+  int i, j, k; //contadores
+  //=====int *vetor_aux;
+
+  //=====vetor_aux = (int *) malloc(sizeof(int) * dir+1); //alocação de memória para o vetor auxiliar
 
   //copiando o vetor no vetor auxiliar
   for(i = meio+1; i>esq; i--) { //primeira metade do vetor
@@ -55,7 +55,7 @@ void insertionSort(int *arr, int n)
 	for (i = 1; i < n; i++) {
 		key = arr[i];
 		j = i - 1;
-    
+
 		while (j >= 0 && arr[j] > key) {
 			arr[j + 1] = arr[j];
 			j = j - 1;
@@ -70,13 +70,14 @@ int main() {
   int i;
   int vetor[] = {12, 6, 16, 5, 20, 2, 10, 7, 19, 4, 8, 15, 22};
   int tamanho_vetor = sizeof(vetor) / sizeof(vetor[0]);
+  int vetor_aux = (int *) malloc(sizeof(int) * tamanho_vetor); //alocação de memória para o vetor auxiliar
 
   printf("vetor antes da ordenacao:\n");
   for(i = 0; i<13; i++) {
     printf("%d ", vetor[i]);
   }
 
-  mergeSort(vetor, 0, tamanho_vetor - 1);
+  mergeSort(vetor, vetor_aux, 0, tamanho_vetor - 1);
 
   printf("\n\nvetor apos a ordenacao:\n");
   for(i = 0; i<13; i++) {
